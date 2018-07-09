35cb82934d915586c18a351ae5a56b04a0538852
Aleksandar Miladinovic <acamiladinovic95@hotmail.com>
2018-07-09 20:41:06 +0200

    add model Teacher

---
+++ src/main/java/vp/spring/rcs/model/user/Teacher.java
@@ -0,0 +1,39 @@
+package vp.spring.rcs.model.user;
+
+import javax.persistence.GeneratedValue;
+
+public class Teacher extends SecurityUser {
+	
+	@GeneratedValue
+	private int citizenID;
+	
+	private String role;
+	
+	public Teacher(int id, String username, String password, String firstName, String lastName, int citizenID, String role) {
+		super(id, username, password, firstName, lastName);
+		this.citizenID = citizenID;
+		this.role = role;
+		// TODO Auto-generated constructor stub
+	}
+
+	public Teacher() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public int getCitizenID() {
+		return citizenID;
+	}
+
+	public void setCitizenID(int citizenID) {
+		this.citizenID = citizenID;
+	}
+
+	public String getRole() {
+		return role;
+	}
+
+	public void setRole(String role) {
+		this.role = role;
+	}
+
+}
