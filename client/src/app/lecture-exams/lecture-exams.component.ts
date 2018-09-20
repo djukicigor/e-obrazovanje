7a977660306520a226628c228ae15840acfb3f02
Igor Djukic <igordjukic0@gmail.com>
2018-09-20 04:39:32 +0200

    admin can apply exam

--- client/src/app/lecture-exams/lecture-exams.component.ts
+++ client/src/app/lecture-exams/lecture-exams.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
-import { SubjectLecturesInterface, User, PassingExamsInterface } from '../common.models';
+import { SubjectLecturesInterface, User, PassingExamsInterface, SubjectInterface } from '../common.models';
 import { UserService } from '../main/user.service';
 import { SubjectService } from '../main/subject.service';
 import { Router } from '@angular/router';
+import { AuthenticationService } from '../security/authentication.service';
 
 @Component({
   selector: 'app-lecture-exams',
@@ -16,8 +17,9 @@ export class LectureExamsComponent implements OnInit {
   public subjectLectures: SubjectLecturesInterface[];
   date: Date;
   public passingExams: PassingExamsInterface[];
+  public teachers: User[];
 
-  constructor(private userService:  UserService, private examService: SubjectService, private router: Router) {
+  constructor(private userService:  UserService, private examService: SubjectService, private router: Router, private authService: AuthenticationService) {
     this.loadData();
   }
 
@@ -27,18 +29,39 @@ export class LectureExamsComponent implements OnInit {
   private loadData(){
     let currentUser = JSON.parse(localStorage.getItem('currentUser'));
     this.isDataAvailable = false;
-    this.userService.getUser(currentUser.username)
-      .subscribe((user: User) => {
-        this.user = user;
-        this.subjectLectures = user.subjectLectures;
-        this.passingExams = user.passingExams;
-        this.isDataAvailable = true;
+    this.subjectLectures = [];
+    if (this.authService.isAdmin()) {
+      this.userService.getAllTeachers()
+      .subscribe((teachers: User[]) => {
+        this.teachers = teachers;
+        teachers.forEach(teacher => {
+          this.subjectLectures = this.subjectLectures.concat(teacher.subjectLectures)
+        });
       })
+    } else {
+      this.userService.getUser(currentUser.username)
+        .subscribe((user: User) => {
+          this.user = user;
+          this.subjectLectures = user.subjectLectures;
+          this.passingExams = user.passingExams;
+          this.isDataAvailable = true;
+        })
+    }
   }
 
   apply(exam): void {
     exam.date = new Date(this.date).getTime();
-    exam.teacher = this.user;
+    if (this.authService.isAdmin()) {
+      this.teachers.forEach(teacher => {
+        teacher.subjectLectures.forEach(sub => {
+          if (sub.subject.name === exam.subject.name) {
+            exam.teacher = teacher;
+          }
+        });
+      });
+    } else {
+      exam.teacher = this.user;
+    }
     if (isNaN(exam.date)) {
       alert('You have to set time before you can apply.')
     } else if (exam.date < Date.now()) {
@@ -54,5 +77,9 @@ export class LectureExamsComponent implements OnInit {
 
   details = function (id) {
     this.router.navigate(['/lecture',id]);
+  }
+
+  isAdmin = () => {
+    return this.authService.isAdmin();
   }
 }
